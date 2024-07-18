65f87fc7e83735f62e63c58a98df9fa124925308
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-18 20:23:41 -0300

    I hope nothing breaks

--- core/src/main/kotlin/com/github/dryingcore/dragonsempire/screen/GameScreen.kt
+++ core/src/main/kotlin/com/github/dryingcore/dragonsempire/screen/GameScreen.kt
@@ -3,24 +3,34 @@ package com.github.dryingcore.dragonsempire.screen
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.utils.Scaling
 import com.badlogic.gdx.utils.viewport.ExtendViewport
+import com.github.dryingcore.dragonsempire.component.ImageComponent
+import com.github.dryingcore.dragonsempire.system.RenderSystem
+import com.github.quillraven.fleks.World
 import ktx.app.KtxScreen
+import ktx.assets.disposeSafely
 import ktx.log.logger
 
 class GameScreen: KtxScreen {
     private val stage: Stage = Stage(ExtendViewport(16f, 9f))
     private val texture: Texture = Texture("assets/graphics/__Run.gif")
+    private val world: World = World {
+        inject(stage)
+        componentListener<ImageComponent.Companion.ImageComponentListener>()
+
+        system<RenderSystem>()
+    }
 
     override fun show() {
         log.debug { "GameScreen is running" }
-        stage.addActor(
-            Image(texture).apply {
-                setPosition(1f, 1f)
-                setSize(1f, 1f)
-                setScaling(Scaling.fill)
+
+        world.entity {
+            add<ImageComponent> {
+                image = Image(texture).apply {
+                    setSize(4f, 4f)
+                }
             }
-        )
+        }
     }
 
     override fun resize(width: Int, height: Int) {
@@ -28,15 +38,13 @@ class GameScreen: KtxScreen {
     }
 
     override fun render(delta: Float) {
-        with(stage) {
-            act(delta)
-            draw()
-        }
+        world.update(delta)
     }
 
     override fun dispose() {
-        stage.dispose()
-        texture.dispose()
+        stage.disposeSafely()
+        texture.disposeSafely()
+        world.dispose()
     }
 
     companion object {
---
+++ core/src/main/kotlin/com/github/dryingcore/dragonsempire/system/RenderSystem.kt
@@ -0,0 +1,33 @@
+package com.github.dryingcore.dragonsempire.system
+
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.github.dryingcore.dragonsempire.component.ImageComponent
+import com.github.quillraven.fleks.AllOf
+import com.github.quillraven.fleks.ComponentMapper
+import com.github.quillraven.fleks.Entity
+import com.github.quillraven.fleks.IteratingSystem
+import com.github.quillraven.fleks.collection.compareEntity
+
+@AllOf([ImageComponent::class])
+class RenderSystem(
+    private val stage: Stage,
+    private val imageCmps: ComponentMapper<ImageComponent>
+): IteratingSystem(
+    comparator = compareEntity {e1,e2 -> imageCmps[e1].compareTo(imageCmps[e2]) }
+) {
+
+    override fun onTick() {
+        super.onTick()
+
+        with(stage) {
+            viewport.apply()
+            act(deltaTime)
+            draw()
+        }
+    }
+
+    override fun onTickEntity(entity: Entity) {
+        imageCmps[entity].image.toFront()
+    }
+
+}
