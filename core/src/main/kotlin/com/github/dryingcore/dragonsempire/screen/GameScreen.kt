28ab80d78a3ec52b8c7626a40c8b824c9414e735
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-18 17:42:17 -0300

    17:43

--- core/src/main/kotlin/com/github/dryingcore/dragonsempire/screen/GameScreen.kt
+++ core/src/main/kotlin/com/github/dryingcore/dragonsempire/screen/GameScreen.kt
@@ -1,12 +1,42 @@
 package com.github.dryingcore.dragonsempire.screen
 
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.utils.Scaling
+import com.badlogic.gdx.utils.viewport.ExtendViewport
 import ktx.app.KtxScreen
 import ktx.log.logger
 
 class GameScreen: KtxScreen {
+    private val stage: Stage = Stage(ExtendViewport(16f, 9f))
+    private val texture: Texture = Texture("assets/graphics/__Run.gif")
 
     override fun show() {
         log.debug { "GameScreen is running" }
+        stage.addActor(
+            Image(texture).apply {
+                setPosition(1f, 1f)
+                setSize(1f, 1f)
+                setScaling(Scaling.fill)
+            }
+        )
+    }
+
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
+    }
+
+    override fun render(delta: Float) {
+        with(stage) {
+            act(delta)
+            draw()
+        }
+    }
+
+    override fun dispose() {
+        stage.dispose()
+        texture.dispose()
     }
 
     companion object {
